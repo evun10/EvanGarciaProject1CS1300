2dd201317ee632f80f22220243f4c45f7ff7490c
evun <EvanGarcia535@gmail.com>
2025-09-09 14:20:45 -0400

    Add files via upload
    added function draw_eyes, draw_nose, and draw_mouth. Which will do exactly what it defined as and utilizes the turtle module.

--- EvanGarciaProject1.py
+++ EvanGarciaProject1.py
@@ -27,11 +27,11 @@ def draw_filled_square(t, size, color):
         t.right(90)
     t.end_fill()
 
-# function draw_filled_squaere will draw a filled triangle!
+# function draw_ears will draw a filled triangle!
 # t is the turtle.
 # size is the size of the filled triangle.
 # color is the color of the filled triangle.
-def draw_filled_triangle(t, size, color):
+def draw_ears(t, size, color):
     t.fillcolor(color)
     t.begin_fill()
     for i in range(3):
@@ -39,18 +39,75 @@ def draw_filled_triangle(t, size, color):
         t.right(120)
     t.end_fill()
 
+# function draw_eyes draws the eyes for the dog!
+# t is the turtle.
+# radius is the radius for the eyes because "t" the turtle is utilizing .circle in the turtle module.
+# x is the x position of the eyes.
+# y is the y position of the eyes.
+# color is the color for the eyes.
+def draw_eyes(t, radius, x, y, color):
+    t.up()
+    t.goto(x, y)
+    t.down()
+    t.fillcolor(color)
+    t.begin_fill()
+    t.circle(radius)
+    t.end_fill()
+
+# function draw_eyes draws the nose for the dog!
+# t is the turtle.
+# size is the size of the nose.
+# x is the x positon for the nose. 
+# y is the y position for the nose. 
+# color is the color for the nose. 
+def draw_nose(t, size, x, y, color):
+    t.up()
+    t.goto(x, y)
+    t.down()
+    t.fillcolor(color)
+    t.begin_fill()
+    for i in range(3):
+        t.forward(size)
+        t.right(120)
+    t.end_fill()
+
+# function draw_mouth will draw the mouth for the dog!
+# t is the turtle.
+# x is the x position for the mouth.
+# y is the y positon for the mouth.
+def draw_mouth(t, x, y):
+    t.up()
+    t.goto(x, y)
+    t.down()
+    t.forward(100)
+
 # main function that will run our application.
 def main():
+    # draw_filled_square is the face of the dog.
     draw_filled_square(t, 500, "tan")
 
+    # creates the right ear.
     t.up()
     t.goto(500,0)
     t.down()
-    draw_filled_triangle(t, 200, "tan")
+    draw_ears(t, 200, "tan")
 
+    # creates the left ear.
     t.up()
     t.goto(-200,0)
     t.down()
-    draw_filled_triangle(t, 200, "tan")
+    draw_ears(t, 200, "tan")
+
+    # creates both eyes.
+    draw_eyes(t, 50, 150, -150, "white")
+    draw_eyes(t, 50, 350, -150, "white")
+
+    # creates the pupils for the eyes!
+    draw_eyes(t, 30, 150, -150, "black")
+    draw_eyes(t, 30, 350, -150, "black")
+
+    draw_nose(t, 150, 180, -250, "black")
+
+    draw_mouth(t, 100, -400)
     turtle.done()
 main()
